782ba197e33773a62b3c3803674d9e966c0eb860
Sarjeet Singh <sarjeetsingh4680@gmail.com>
2024-01-21 19:53:08 +0530

    corrected class to className at some positions

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
 
 
-  const generatePassword = () => {
+  const generatePassword = useCallback(() => {
     let password = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     let modifiedStr = str;
@@ -29,7 +29,7 @@ function App() {
 
     setPassword(password);
     console.log(password);
-  };
+  },[isChars,isNums,setPassword]);
 
 
   useEffect(() => { generatePassword() }, [count,isChars,isNums])
@@ -86,41 +86,41 @@ function App() {
             <span className="ml-2 font-light">{count}</span>
           </div>
 
-          <div class="inline-flex items-center">
-            <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="check">
+          <div className="inline-flex items-center">
+            <label className="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="check">
               <input type="checkbox"
-                class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+                className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
                 id="nums" onChange={() => { setIsNums((prev) => !prev) }}  />
               <span
-                class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
-                  stroke="currentColor" stroke-width="1">
-                  <path fill-rule="evenodd"
+                className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
+                  stroke="currentColor" strokeWidth="1">
+                  <path fillRule="evenodd"
                     d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clip-rule="evenodd"></path>
+                    clipRule="evenodd"></path>
                 </svg>
               </span>
             </label>
-            <label class="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="nums">
+            <label className="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="nums">
               Numbers
             </label>
           </div>
-          <div class="inline-flex items-center">
-            <label class="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="check">
+          <div className="inline-flex items-center">
+            <label className="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="check">
               <input type="checkbox"
-                class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+                className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
                 id="check" onChange={() => { setIsChars((prev) => !prev) }}  />
               <span
-                class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
-                  stroke="currentColor" stroke-width="1">
-                  <path fill-rule="evenodd"
+                className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
+                  stroke="currentColor"  strokeWidth="1">
+                  <path fillRule="evenodd"
                     d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clip-rule="evenodd"></path>
+                    clipRule="evenodd"></path>
                 </svg>
               </span>
             </label>
-            <label class="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="check">
+            <label className="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="check">
               Special Chars
             </label>
           </div>
